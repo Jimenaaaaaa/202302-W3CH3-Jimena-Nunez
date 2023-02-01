c83975767e99e7a0ae67e920e9714ec2d9decad5
Jimena <jimenanrp2@gmail.com>
2023-02-01 21:05:21 +0100

    pet mock and pet model

--- src/components/pets/pets.ts
+++ src/components/pets/pets.ts
@@ -1,1 +1,27 @@
-export class Pets 
+import { Pet, PetType } from '../../models/pets';
+import { Component } from '../component/component';
+
+export class Pets extends Component {
+  constructor(public selector: string, public pets: PetType[]) {
+    super();
+    this.template = this.createTemplate();
+    this.render('afterbegin');
+  }
+
+  private createTemplate() {
+    const items = this.pets
+      .map(
+        (item) => `
+        <li class="card">
+          <span>${item.isCompleted}</span>
+          <span title="${item.id}">${item.name}</span>
+          <span>${item.owner}</span>
+          <button>🗑️</button>
+        </li>`
+      )
+      .join('\n');
+    return `
+    <section class="tasks"><ul>${items}</ul></section>
+    `;
+  }
+}
---
+++ src/mocks/pet.ts
@@ -0,0 +1,7 @@
+import { Pet, PetType } from "../models/pets";
+
+export const PETS: PetType[] = [
+  new Pet('Aliki', 'mixed race', 'Jimena'),
+  new Pet('Bambu', 'cat', 'Rodri'),
+  new Pet('Fredy', 'galgo', 'Alvaro'),
+];
